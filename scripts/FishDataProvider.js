a8fd78d94983df07b006cbcbd07a8ae0f9f5a577
Steve Brownlee <chortlehoort@gmail.com>
2020-07-14 10:34:03 -0500

    Added fish data provider

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,26 @@
+const fishCollection = [
+    {
+        name: "Dory",
+        food: "Plankton",
+        species: "Blue Tang",
+        length: 4,
+        location: "Great Barrier Reef",
+        image: "./images/bluetang.jpeg"
+    },
+    {
+        name: "Nemo",
+        food: "Plankton, Algae",
+        species: "Clown Fish",
+        length: 5,
+        location: "Great Barrier Reef",
+        image: "./images/clownfish.jpeg"
+    },
+    {
+        name: "Bruce",
+        food: "Fish, Tires",
+        species: "Great White Shark",
+        length: 25,
+        location: "Great Barrier Reef",
+        image: "./images/nado.jpeg"
+    }
+]
