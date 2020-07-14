f64ecfaedfa9e55b6710ca549e7b38c49090c7a6
Kristen Norris <krnorris65@gmail.com>
2020-07-14 13:34:54 -0500

    created fish component and fish list component

---
+++ scripts/FishList.js
@@ -0,0 +1,14 @@
+// this module creates the list of fish to add to the DOM
+import {useFish} from "./FishDataProvider.js"
+
+export const FishList = () => {
+    const contentElement = document.querySelector(".content--left")
+    const fishes = useFish()
+
+    contentElement.innerHTML += `
+        <article class="fishList">
+            All the fish go here!
+        </article>
+    `
+
+}
