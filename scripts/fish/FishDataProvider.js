55c4c0df2915a36d3b5b7ef27cab4670433f3897
Steve Brownlee <chortlehoort@gmail.com>
2020-07-15 08:40:26 -0500

    Added more fish and some quote styling

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -22,6 +22,54 @@ const fishCollection = [
         length: 25,
         location: "Great Barrier Reef",
         image: "./images/fish/nado.jpeg"
+    },
+    {
+        name: "Dory",
+        food: "Plankton",
+        species: "Blue Tang",
+        length: 4,
+        location: "Great Barrier Reef",
+        image: "./images/fish/bluetang.jpeg"
+    },
+    {
+        name: "Nemo",
+        food: "Plankton, Algae",
+        species: "Clown Fish",
+        length: 5,
+        location: "Great Barrier Reef",
+        image: "./images/fish/clownfish.jpeg"
+    },
+    {
+        name: "Bruce",
+        food: "Fish, Tires",
+        species: "Great White Shark",
+        length: 25,
+        location: "Great Barrier Reef",
+        image: "./images/fish/nado.jpeg"
+    },
+    {
+        name: "Dory",
+        food: "Plankton",
+        species: "Blue Tang",
+        length: 4,
+        location: "Great Barrier Reef",
+        image: "./images/fish/bluetang.jpeg"
+    },
+    {
+        name: "Nemo",
+        food: "Plankton, Algae",
+        species: "Clown Fish",
+        length: 5,
+        location: "Great Barrier Reef",
+        image: "./images/fish/clownfish.jpeg"
+    },
+    {
+        name: "Bruce",
+        food: "Fish, Tires",
+        species: "Great White Shark",
+        length: 25,
+        location: "Great Barrier Reef",
+        image: "./images/fish/nado.jpeg"
     }
 ]
 
--- scripts/quotes/QuoteConverter.js
+++ scripts/quotes/QuoteConverter.js
@@ -1,8 +1,8 @@
 export const QuoteAsHTML = (quoteObject) => {
     return `
         <section class="quote card">
-            <div class="quote__author">${quoteObject.author}</div>
             <div class="quote__text">${quoteObject.text}</div>
+            <div class="quote__author">${quoteObject.author}</div>
         </section>
     `
 }
